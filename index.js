74162b905a4f3ea19ebacca89d0c197e81ea196d
ssmahim01 <mahimzayn666@gmail.com>
2024-12-23 06:10:52 +0600

    Created a database with its collection then completed get and post of CRUD operation

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ybs8l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +26,23 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. Successfully connected to MongoDB!");
+
+    const carCollection = client.db("carsDB").collection("cars");
+
+    app.get("/cars", async(req, res) => {
+      const findData = carCollection.find();
+      const convertToArray = await findData.toArray();
+      res.send(convertToArray);
+    });
+
+    app.post("/cars", async(req, res) => {
+      const carsData = req.body;
+      // console.log(carsData);
+
+      const insertResult = await carCollection.insertOne(carsData);
+      res.send(insertResult);
+    });
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
