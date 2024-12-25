5125b922493fdbee664f214d99618b1e540f3f0d
ssmahim01 <mahimzayn666@gmail.com>
2024-12-26 00:15:57 +0600

    Added JSON file of vercel then deployed the server on vercel and Modified JS file of index with .gitignore file

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const port = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://car-rental-client.web.app",
+      "https://car-rental-client.firebaseapp.com",
+    ],
     credentials: true,
   })
 );
@@ -50,10 +54,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. Successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. Successfully connected to MongoDB!");
 
     const carCollection = client.db("carsDB").collection("cars");
     const bookCollection = client.db("carsDB").collection("bookedCars");
@@ -70,7 +74,8 @@ async function run() {
       res
         .cookie("token", token, {
           httpOnly: true,
-          secure: false,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
         .send({ success: true });
     });
@@ -79,7 +84,8 @@ async function run() {
       res
         .clearCookie("token", {
           httpOnly: true,
-          secure: false,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
         .send({ success: true });
     });
@@ -250,7 +256,6 @@ async function run() {
       const result = await bookCollection.updateOne(query, modifyDates);
       res.send(result);
     });
-
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
